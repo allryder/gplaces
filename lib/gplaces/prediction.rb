a571394fc12e1a17b2e3bd1c4070c5f010193f38
Klaus Hartl <kh@waymate.de>
2014-06-24 17:06:36 +0200

    Adapt Prediction to support place_id.
    Documenation states that the id and reference fields are deprecated as of June 24, 2014 (today),
    and are replaced by by the new place_id field.

--- lib/gplaces/prediction.rb
+++ lib/gplaces/prediction.rb
@@ -1,10 +1,11 @@
 module Gplaces
   class Prediction
     include Virtus.model
-    attribute :description, String
-    attribute :reference , String
-    attribute :id, String
-    attribute :terms, Array
+    attribute :description,        String
+    attribute :place_id,           String
+    attribute :reference ,         String # deprecated as of June 24, 2014, replaced by place_id
+    attribute :id,                 String # deprecated as of June 24, 2014, replaced by place_id
+    attribute :terms,              Array
     attribute :matched_substrings, Array
   end
 end
--- spec/lib/prediction_spec.rb
+++ spec/lib/prediction_spec.rb
@@ -1,25 +1,38 @@
 require 'spec_helper'
 
 describe Gplaces::Prediction do
-  subject { described_class.new(description: 'desc', reference: 'ref', id: 'id', terms: [], matched_substrings: ['subs'])}
+  def prediction
+    described_class.new(
+      description:        'description',
+      place_id:           'place_id',
+      reference:          'reference',
+      id:                 'id',
+      terms:              [{}],
+      matched_substrings: [{}]
+    )
+  end
 
   it 'has a description' do
-    expect(subject.description).to eq('desc')
+    expect(prediction.description).to eq('description')
   end
 
-  it 'has a reference' do
-    expect(subject.reference).to eq('ref')
+  it 'has a place_id' do
+    expect(prediction.place_id).to eq('place_id')
   end
 
-  it 'has an id' do
-    expect(subject.id).to eq('id')
+  it 'has a reference (deprecated as of June 24, 2014)' do
+    expect(prediction.reference).to eq('reference')
+  end
+
+  it 'has an id (deprecated as of June 24, 2014)' do
+    expect(prediction.id).to eq('id')
   end
 
   it 'has terms' do
-    expect(subject.terms).to eq([])
+    expect(prediction.terms).to eq([{}])
   end
 
   it 'has matched_substrings' do
-    expect(subject.matched_substrings).to eq(['subs'])
+    expect(prediction.matched_substrings).to eq([{}])
   end
 end
