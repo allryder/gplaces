7e4fb3473bf00d8d89b4d28942b52976a28fb6c6
Klaus Hartl <klaus@allyapp.com>
2015-06-25 16:00:52 +0200

    Fix test to use location directly

--- spec/lib/gplaces/place_spec.rb
+++ spec/lib/gplaces/place_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Gplaces::Place do
   end
 
   it "has lat and lng" do
-    expect(subject.geometry.location.lat).to eq(-33.8669330)
-    expect(subject.geometry.location.lng).to eq(151.1957910)
+    expect(subject.location.lat).to eq(-33.8669330)
+    expect(subject.location.lng).to eq(151.1957910)
   end
 end
