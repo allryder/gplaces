14ab1b36372b037c76517157b10cece1ad1c5a53
Klaus Hartl <klaus@allyapp.com>
2015-07-30 10:03:07 +0200

    Switch http client from faraday to using curb
    For performance reasons.

--- gplaces.gemspec
+++ gplaces.gemspec
@@ -17,7 +17,7 @@ Gem::Specification.new do |gem|
   gem.test_files    = gem.files.grep(%r{^(test|spec|features)/})
   gem.require_paths = ["lib"]
 
-  gem.add_dependency "faraday"
+  gem.add_dependency "curb"
 
   gem.add_development_dependency "rake", "~> 10.4"
   gem.add_development_dependency "rspec", "~> 3.0"
--- lib/gplaces.rb
+++ lib/gplaces.rb
@@ -1,8 +1,4 @@
 module Gplaces
-  BASE_URI          = "https://maps.googleapis.com".freeze
-  AUTOCOMPLETE_URI  = "/maps/api/place/autocomplete/json".freeze
-  PLACE_DETAILS_URI = "/maps/api/place/details/json".freeze
-
   autoload :VERSION,             "gplaces/version"
   autoload :Client,              "gplaces/client"
   autoload :Prediction,          "gplaces/prediction"
